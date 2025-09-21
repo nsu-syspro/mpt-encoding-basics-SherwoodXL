75c612a85955e4f081c4e6b456d3ebe9c03086ff
SherwoodXL <sherwoodgl@HOME-PC.localdomain>
2025-09-21 21:09:00 +0700

    Update 1.0

---
+++ solution/task1.sh
@@ -0,0 +1,3 @@
+iconv -f KOI8-R -t UTF-8 solution/task2.sh -o solution/task2.sh
+iconv -f IBM866 -t UTF-8 solution/task3.sh -o solution/task3.sh
+iconv -f ISO-8859-5 -t UTF-8 solution/task4.sh -o solution/task4.sh
